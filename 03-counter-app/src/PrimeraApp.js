33b9dc197589390e5b6f5b01b350905ce1c0aac7
Alex <axherrerax@gmail.com>
2021-09-22 13:40:17 -0300

    Primera App

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,37 @@
+
+
+const PrimeraApp = ({saludo ="Hola mudno otra vez"}) => {
+
+
+
+
+    return (
+        <>
+            <h1> {saludo}</h1>
+
+            
+        
+        </>
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default PrimeraApp;
+
+
+
+
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,1 +1,22 @@
-console.log("Prueba de actualizacion automatica")
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+import './index.css'
+
+
+
+
+
+
+const divRoot = document.querySelector('#root')
+
+
+ReactDOM.render(<PrimeraApp  />, divRoot)
+
+
+
+
+
+
+
+
