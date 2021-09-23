c7dbe35ef539a5ab3cda001760cb31322edc963a
Alex <axherrerax@gmail.com>
2021-09-23 01:54:45 -0300

    Primera prueba con Jest

---
+++ 03-counter-app/src/tests/demo.test.js
@@ -0,0 +1,25 @@
+describe('Pruebas en el archivo demo.test.js', () =>{
+
+    test ('Los string deben ser iguales', () =>{
+    
+        // 1. Inicializacion 
+        const mensaje="Hola mundo"
+    
+    
+        // 2. Estimulo
+        const mensaje2=`Hola mundo`
+    
+        // 3. Observar el comporamiento
+    
+        expect(mensaje).toBe(mensaje2) //===
+       
+    })
+    
+    test('should ', () => {
+        
+    })
+});
+
+
+
+
